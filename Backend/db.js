3bff15bd92d898b19f2bbee246b8456fb2f46eaf
Abhay-Rana-18 <ranaabhay795@gmail.com>
2023-11-13 22:01:08 +0530

    "mongodb atlas"

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,9 @@
-
+console.log("start");
 const mongoose = require('mongoose');
-
-const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
+console.log("middle");
+// const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
+const mongoURI = 'mongodb+srv://abhay18:lvxpPiCuRe0n2wEF@atlascluster.vmitatq.mongodb.net/eshopping?retryWrites=true&w=majority';
+console.log("end");
 
 const connectToMongo = async() => {
     await mongoose.connect(mongoURI);
