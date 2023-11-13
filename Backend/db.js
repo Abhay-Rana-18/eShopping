26595799d90ab426379c18925490ffcf60f6f20b
Abhay-Rana-18 <ranaabhay795@gmail.com>
2023-11-13 20:56:58 +0530

    "FEW CHANGES"

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,7 @@
+
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/eshopping?directConnection=true&serverSelectionTimeoutMS=5000&appName=mongosh+1.10.1';
+const mongoURI = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
 
 const connectToMongo = async() => {
     await mongoose.connect(mongoURI);
--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 var cors = require('cors');
 
 connectToMongo();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
@@ -17,4 +17,4 @@ app.use('/eShopping/atc', require('./Routes/atcRoute'));
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}/`);
-})
+});
